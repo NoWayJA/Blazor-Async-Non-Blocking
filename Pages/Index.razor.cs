dedfff7b29d2a650221ed3d627b9b701d4337e3a
NoWayJA <{ID}+{username}@users.noreply.github.com>
2022-10-19 18:52:19 +0100

    simple await example

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+namespace Good_Bad_Awaiting.Pages
+{
+    public partial class Index
+    {
+        private string YourName = "Mr Blobby";
+        private string block1 = "";
+        private string block2 = "";
+        private string block3 = "";
+        private string block4 = "";
+        private string block5 = "";
+        private string block6 = "";
+        public Index()
+        {
+        }
+        private async Task Master1()
+        {
+            ClearBlocks();
+            await Block1();
+            await Block2();
+            await Block3();
+            await Block4();
+            await Block5();
+            await Block6();
+        }
+
+        private async Task Master2()
+        {
+            ClearBlocks();
+            Block1();
+            Block2();
+            Block3();
+            Block4();
+            Block5();
+            Block6();
+        }
+
+
+
+        private void ClearBlocks()
+        {
+             block1 = "";
+             block2 = "";
+             block3 = "";
+             block4 = "";
+             block5 = "";
+             block6 = "";
+            StateHasChanged();
+        }
+
+        private async Task Block1()
+        {
+            await Task.Delay(1000);
+            block1 = "block1 set";
+            StateHasChanged();
+        }
+        private async Task Block2()
+        {
+            await Task.Delay(1000);
+            block2 = "block2 set";
+            StateHasChanged();
+        }
+        private async Task Block3()
+        {
+            await Task.Delay(1000);
+            block3 = "block3 set";
+            StateHasChanged();
+        }
+        private async Task Block4()
+        {
+            await Task.Delay(1000);
+            block4 = "block4 set";
+            StateHasChanged();
+        }
+        private async Task Block5()
+        {
+            await Task.Delay(1000);
+            block5 = "block5 set";
+            StateHasChanged();
+        }
+        private async Task Block6()
+        {
+            await Task.Delay(1000);
+            block6 = "block6 set";
+            StateHasChanged();
+        }
+    }
+}
+
---
+++ README.txt
@@ -0,0 +1,7 @@
+﻿This project shows the use of Async and Await in blazor
+
+In one way your Async calls form a traffic jam and queue up behind each other
+In the other example, all your Async calls happen in parallel (much faster)
+
+Awaits when used badly are one of the biggest causes of slow code, this project aims to give you a simple example of Awaits working well and working badly.
+
